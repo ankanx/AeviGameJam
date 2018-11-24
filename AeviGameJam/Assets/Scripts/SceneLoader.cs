4f1cb2b77e85659956a46d8d21aac228a38161da
Andreas Fransson <Ankanx@gmail.com>
2018-11-24 13:35:41 +0100

    Fixed time scale bug and some text

--- AeviGameJam/Assets/Scripts/SceneLoader.cs
+++ AeviGameJam/Assets/Scripts/SceneLoader.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
+using TMPro;
 public class SceneLoader : MonoBehaviour {
 
     public static SceneLoader Instance { get; set; }
     public GameObject LoadingScreenUI;
     public bool isLoading = false;
     public Animator loadinganim;
+    public TextMeshProUGUI loadingText;
 
     private void Start()
     {
@@ -26,6 +27,7 @@ public class SceneLoader : MonoBehaviour {
 
     public void LoadScene (int SceneIndex,SaveData save)
     {
+        Time.timeScale = 1f;
         LoadingScreenUI.GetComponent<Canvas>().enabled = true;
         StartCoroutine(LoadAsync(SceneIndex,save));
     }
@@ -33,16 +35,17 @@ public class SceneLoader : MonoBehaviour {
     IEnumerator LoadAsync (int SceneIndex, SaveData save)
     {
         isLoading = true;
-
+        loadingText.text = "Loading";
         AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
 
         while (!operation.isDone)
         {
-
+            loadingText.text = loadingText.text + ".";
             yield return null;
         }
         yield return new WaitForSeconds(3);
         loadinganim.SetTrigger("Finished");
+        loadingText.text = "";
         yield return new WaitForSeconds(1);
         loadinganim.SetTrigger("Done");
         LoadingScreenUI.GetComponent<Canvas>().enabled = false;
