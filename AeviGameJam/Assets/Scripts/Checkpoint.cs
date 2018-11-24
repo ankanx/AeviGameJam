0d666388b12265be4eed5a15bd5b590d74373851
Andreas Fransson <Ankanx@gmail.com>
2018-11-24 21:36:53 +0100

    checkpoint

--- AeviGameJam/Assets/Scripts/Checkpoint.cs
+++ AeviGameJam/Assets/Scripts/Checkpoint.cs
@@ -6,7 +6,7 @@ public class Checkpoint : MonoBehaviour {
 
     private GameHandler GM;
     bool isTriggered = false;
-
+    public Animator particles;
     private void Start()
     {
         GM = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<GameHandler>();
@@ -19,6 +19,7 @@ public class Checkpoint : MonoBehaviour {
             GM.LastCheckPointPos = transform.position;
             //GetComponent<SpriteRenderer>().color = new Color(255,0,0,255);
             GetComponent<Animator>().SetTrigger("ColorChange");
+            particles.SetTrigger("Spin");
             isTriggered = true;
         }
     }
